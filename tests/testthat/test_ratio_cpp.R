a7d68202203b0a1ffce03a767c0892bce12ec1f5
Junya Watanabe <jw2098@cam.ac.uk>
2022-12-30 20:09:23 +0000

    Draft test coef_wise

--- tests/testthat/test_ratio_cpp.R
+++ tests/testthat/test_ratio_cpp.R
@@ -3,7 +3,7 @@ tol <- 5e-7
 test_that("Expect equal from R and Cpp methods: qfrm", {
     nvs <- 2:4
     ks <- c(1:3, 1/2)
-    m <- 50
+    m <- 20
     for(nv in nvs) {
         L1 <- 1:nv
         L2 <- nv:1
@@ -42,7 +42,7 @@ test_that("Expect equal from R and Cpp methods: qfrm", {
 test_that("Expect equal from R and Cpp methods: qfmrm", {
     nvs <- 2:4
     ks <- c(1, 2, 1/2)
-    m <- 25
+    m <- 20
     for(nv in nvs) {
         L1 <- 1:nv
         L2 <- nv:1
@@ -130,7 +130,7 @@ test_that("Expect equal from double and long double in C++: qfrm", {
 test_that("Expect equal from double and long double in C++: qfmrm", {
     nvs <- 2:4
     ks <- c(1, 2, 1/2)
-    m <- 25
+    m <- 20
     for(nv in nvs) {
         L1 <- 1:nv
         L2 <- nv:1
@@ -173,6 +173,94 @@ test_that("Expect equal from double and long double in C++: qfmrm", {
                                  qfmrm(A1, A2r, A3, p, q, r, m = m, check_convergence = FALSE, use_cpp = TRUE, cpp_method = "long_double"), tolerance = tol)
                     expect_equal(qfmrm(A1, A2r, A3, p, q, r, m = m, mu = mu, check_convergence = FALSE, use_cpp = TRUE, cpp_method = "double"),
                                  qfmrm(A1, A2r, A3, p, q, r, m = m, mu = mu, check_convergence = FALSE, use_cpp = TRUE, cpp_method = "long_double"), tolerance = tol)
+                }
+            }
+        }
+    }
+})
+
+test_that("Expect equal from double and coef_wise in C++: qfrm", {
+    nvs <- 2:4
+    ks <- c(1:3, 1/2, 3/2)
+    m <- 50
+    for(nv in nvs) {
+        L1 <- 1:nv
+        L2 <- nv:1
+        # L3 <- sqrt(nv:1)
+        A1 <- diag(L1)
+        A2 <- diag(L2)
+        # A3 <- diag(L3)
+        I <- diag(nv)
+        mu <- 1:nv / nv
+        Q <- qr.Q(qr(matrix(stats::rnorm(nv^2), nv, nv)))
+        # A1r <- Q %*% A1 %*% t(Q)
+        A2r <- Q %*% A2 %*% t(Q)
+        # A3r <- Q %*% A3 %*% t(Q)
+
+        for(p in ks[ks %% 1 != 0]) {
+            for(q in ks) {
+                if(nv / 2 + p <= q) next
+                expect_equal(suppressWarnings(qfrm(A1, I,  p, q, m = m, mu = mu, check_convergence = FALSE, use_cpp = TRUE, cpp_method = "double")),
+                             suppressWarnings(qfrm(A1, I,  p, q, m = m, mu = mu, check_convergence = FALSE, use_cpp = TRUE, cpp_method = "coef_wise")), tolerance = tol)
+                expect_equal(qfrm(A1, A2,  p, q, m = m, check_convergence = FALSE, use_cpp = TRUE, cpp_method = "double"),
+                             qfrm(A1, A2,  p, q, m = m, check_convergence = FALSE, use_cpp = TRUE, cpp_method = "coef_wise"), tolerance = tol)
+                expect_equal(qfrm(A1, A2,  p, q, m = m, mu = mu, check_convergence = FALSE, use_cpp = TRUE, cpp_method = "double"),
+                             qfrm(A1, A2,  p, q, m = m, mu = mu, check_convergence = FALSE, use_cpp = TRUE, cpp_method = "coef_wise"), tolerance = tol)
+                expect_equal(qfrm(A1, A2r, p, q, m = m, check_convergence = FALSE, use_cpp = TRUE, cpp_method = "double"),
+                             qfrm(A1, A2r, p, q, m = m, check_convergence = FALSE, use_cpp = TRUE, cpp_method = "coef_wise"), tolerance = tol)
+                expect_equal(qfrm(A1, A2r, p, q, m = m, mu = mu, check_convergence = FALSE, use_cpp = TRUE, cpp_method = "double"),
+                             qfrm(A1, A2r, p, q, m = m, mu = mu, check_convergence = FALSE, use_cpp = TRUE, cpp_method = "coef_wise"), tolerance = tol)
+            }
+        }
+    }
+})
+
+test_that("Expect equal from double and coef_wise in C++: qfmrm", {
+    nvs <- 2:4
+    ks <- c(1, 2, 1/2)
+    m <- 20
+    for(nv in nvs) {
+        L1 <- 1:nv
+        L2 <- nv:1
+        L3 <- sqrt(nv:1)
+        A1 <- diag(L1)
+        A2 <- diag(L2)
+        A3 <- diag(L3)
+        I <- diag(nv)
+        mu <- 1:nv / nv
+        Q <- qr.Q(qr(matrix(stats::rnorm(nv^2), nv, nv)))
+        # A1r <- Q %*% A1 %*% t(Q)
+        A2r <- Q %*% A2 %*% t(Q)
+        # A3r <- Q %*% A3 %*% t(Q)
+
+        for(p in ks) {
+            for(q in ks/2) {
+                for(r in ks/2) {
+                    if(nv / 2 + p <= q + r) next
+                    expect_equal(qfmrm(A1, A2, I,  p, q, r, m = m, check_convergence = FALSE, use_cpp = TRUE, cpp_method = "double"),
+                                 qfmrm(A1, A2, I,  p, q, r, m = m, check_convergence = FALSE, use_cpp = TRUE, cpp_method = "coef_wise"), tolerance = tol)
+                    expect_equal(qfmrm(A1, A2, I,  p, q, r, m = m, mu = mu, check_convergence = FALSE, use_cpp = TRUE, cpp_method = "double"),
+                                 qfmrm(A1, A2, I,  p, q, r, m = m, mu = mu, check_convergence = FALSE, use_cpp = TRUE, cpp_method = "coef_wise"), tolerance = tol)
+                    expect_equal(qfmrm(A1, A2r, I, p, q, r, m = m, check_convergence = FALSE, use_cpp = TRUE, cpp_method = "double"),
+                                 qfmrm(A1, A2r, I, p, q, r, m = m, check_convergence = FALSE, use_cpp = TRUE, cpp_method = "coef_wise"), tolerance = tol)
+                    expect_equal(qfmrm(A1, A2r, I, p, q, r, m = m, mu = mu, check_convergence = FALSE, use_cpp = TRUE, cpp_method = "double"),
+                                 qfmrm(A1, A2r, I, p, q, r, m = m, mu = mu, check_convergence = FALSE, use_cpp = TRUE, cpp_method = "coef_wise"), tolerance = tol)
+                    expect_equal(qfmrm(I, A2, A3,  p, q, r, m = m, check_convergence = FALSE, use_cpp = TRUE, cpp_method = "double"),
+                                 qfmrm(I, A2, A3,  p, q, r, m = m, check_convergence = FALSE, use_cpp = TRUE, cpp_method = "coef_wise"), tolerance = tol)
+                    expect_equal(qfmrm(I, A2, A3,  p, q, r, m = m, mu = mu, check_convergence = FALSE, use_cpp = TRUE, cpp_method = "double"),
+                                 qfmrm(I, A2, A3,  p, q, r, m = m, mu = mu, check_convergence = FALSE, use_cpp = TRUE, cpp_method = "coef_wise"), tolerance = tol)
+                    expect_equal(qfmrm(I, A2r, A3, p, q, r, m = m, check_convergence = FALSE, use_cpp = TRUE, cpp_method = "double"),
+                                 qfmrm(I, A2r, A3, p, q, r, m = m, check_convergence = FALSE, use_cpp = TRUE, cpp_method = "coef_wise"), tolerance = tol)
+                    expect_equal(qfmrm(I, A2r, A3, p, q, r, m = m, mu = mu, check_convergence = FALSE, use_cpp = TRUE, cpp_method = "double"),
+                                 qfmrm(I, A2r, A3, p, q, r, m = m, mu = mu, check_convergence = FALSE, use_cpp = TRUE, cpp_method = "coef_wise"), tolerance = tol)
+                    expect_equal(qfmrm(A1, A2, A3,  p, q, r, m = m, check_convergence = FALSE, use_cpp = TRUE, cpp_method = "double"),
+                                 qfmrm(A1, A2, A3,  p, q, r, m = m, check_convergence = FALSE, use_cpp = TRUE, cpp_method = "coef_wise"), tolerance = tol)
+                    expect_equal(qfmrm(A1, A2, A3,  p, q, r, m = m, mu = mu, check_convergence = FALSE, use_cpp = TRUE, cpp_method = "double"),
+                                 qfmrm(A1, A2, A3,  p, q, r, m = m, mu = mu, check_convergence = FALSE, use_cpp = TRUE, cpp_method = "coef_wise"), tolerance = tol)
+                    expect_equal(qfmrm(A1, A2r, A3, p, q, r, m = m, check_convergence = FALSE, use_cpp = TRUE, cpp_method = "double"),
+                                 qfmrm(A1, A2r, A3, p, q, r, m = m, check_convergence = FALSE, use_cpp = TRUE, cpp_method = "coef_wise"), tolerance = tol)
+                    expect_equal(qfmrm(A1, A2r, A3, p, q, r, m = m, mu = mu, check_convergence = FALSE, use_cpp = TRUE, cpp_method = "double"),
+                                 qfmrm(A1, A2r, A3, p, q, r, m = m, mu = mu, check_convergence = FALSE, use_cpp = TRUE, cpp_method = "coef_wise"), tolerance = tol)
                 }
             }
         }
