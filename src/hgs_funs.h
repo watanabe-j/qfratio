f986360a49e92a9f8f291ddb0250f08cecc6b661
Junya Watanabe <jw2098@cam.ac.uk>
2023-01-13 22:30:38 +0000

    Templatize hgs_funs (both order- and coef-wise)

--- src/hgs_funs.h
+++ src/hgs_funs.h
@@ -19,51 +19,40 @@ Eigen::Array<T, Eigen::Dynamic, 1>  get_sign_rf(const T a, const int n);
 template <typename T>
 Eigen::Array<T, Eigen::Dynamic, 1> get_sign_rfp1(const T a, const int n);
 
-Eigen::ArrayXd hgs_1dE(const Eigen::ArrayXd& dks,
-    const double a1,
-    const double b, const double lconst, const Eigen::ArrayXd& lscf);
+template <typename Derived>
+Eigen::Array<typename Derived::Scalar, Eigen::Dynamic, 1>
+hgs_1dE(const Eigen::ArrayBase<Derived>& dks, const typename Derived::Scalar a1, 
+        const typename Derived::Scalar b, const typename Derived::Scalar lconst,
+        const Eigen::ArrayBase<Derived>& lscf);
 
-// template <typename Derived>
-// Eigen::Array<typename Derived::Scalar, Eigen::Dynamic, Eigen::Dynamic>
-// hgs_2dE(const Eigen::ArrayBase<Derived>& dks,
-//         const typename Derived::Scalar a1, const typename Derived::Scalar a2,
-//         const double b, const typename Derived::Scalar lconst,
-//         const Eigen::ArrayBase<Derived>& lscf);
+template <typename Derived>
+Eigen::Array<typename Derived::Scalar, Eigen::Dynamic, Eigen::Dynamic>
+hgs_2dE(const Eigen::ArrayBase<Derived>& dks,
+        const typename Derived::Scalar a1, const typename Derived::Scalar a2,
+        const typename Derived::Scalar b, const typename Derived::Scalar lconst,
+        const Eigen::Array<typename Derived::Scalar, Eigen::Dynamic, 1>& lscf);
 
-Eigen::ArrayXXd
-hgs_2dE(const Eigen::ArrayXXd& dks,
-        const double a1, const double a2, const double b,
-        const double lconst, const Eigen::ArrayXd& lscf);
-Eigen::ArrayXXd
-hgs_2dE(const Eigen::ArrayXXd& dks,
-        const double a1, const double a2, const double b,
-        const double lconst, const Eigen::ArrayXXd& lscf);
-Eigen::Array<long double, Eigen::Dynamic, Eigen::Dynamic>
-hgs_2dE(const Eigen::Array<long double, Eigen::Dynamic, Eigen::Dynamic>& dks,
-        const long double a1, const long double a2, const long double b,
-        const long double lconst,
-        // const Eigen::Array<long double, Eigen::Dynamic, Eigen::Dynamic>& lscf);
-        const Eigen::Array<long double, Eigen::Dynamic, 1>& lscf);
+template <typename Derived>
+Eigen::Array<typename Derived::Scalar, Eigen::Dynamic, Eigen::Dynamic>
+hgs_2dE(const Eigen::ArrayBase<Derived>& dks,
+        const typename Derived::Scalar a1, const typename Derived::Scalar a2,
+        const typename Derived::Scalar b, const typename Derived::Scalar lconst,
+        const Eigen::ArrayBase<Derived>& lscf);
 
-// template <typename Derived>
-// Eigen::Array<typename Derived::Scalar, Eigen::Dynamic, Eigen::Dynamic>
-// hgs_3dE(const Eigen::ArrayBase<Derived>& dks, const typename Derived::Scalar a1,
-//         const typename Derived::Scalar a2, const typename Derived::Scalar a3,
-//         const double b, const typename Derived::Scalar lconst,
-//         const Eigen::ArrayBase<Derived>& lscf);
 
-Eigen::ArrayXXd
-hgs_3dE(const Eigen::ArrayXXd& dks,
-        const double a1, const double a2, const double a3,
-        const double b, const double lconst, const Eigen::ArrayXd& lscf);
-Eigen::ArrayXXd
-hgs_3dE(const Eigen::ArrayXXd& dks,
-        const double a1, const double a2, const double a3,
-        const double b, const double lconst, const Eigen::ArrayXXd& lscf);
-Eigen::Array<long double, Eigen::Dynamic, Eigen::Dynamic>
-hgs_3dE(const Eigen::Array<long double, Eigen::Dynamic, Eigen::Dynamic>& dks,
-        const long double a1, const long double a2, const long double a3,
-        const long double b, const long double lconst,
-        // const Eigen::Array<long double, Eigen::Dynamic, Eigen::Dynamic>& lscf);
-        const Eigen::Array<long double, Eigen::Dynamic, 1>& lscf);
+template <typename Derived>
+Eigen::Array<typename Derived::Scalar, Eigen::Dynamic, Eigen::Dynamic>
+hgs_3dE(const Eigen::ArrayBase<Derived>& dks, const typename Derived::Scalar a1,
+        const typename Derived::Scalar a2, const typename Derived::Scalar a3,
+        const typename Derived::Scalar b, const typename Derived::Scalar lconst,
+        const Eigen::Array<typename Derived::Scalar, Eigen::Dynamic, 1>& lscf);
+
+template <typename Derived>
+Eigen::Array<typename Derived::Scalar, Eigen::Dynamic, Eigen::Dynamic>
+hgs_3dE(const Eigen::ArrayBase<Derived>& dks,
+        const typename Derived::Scalar a1, const typename Derived::Scalar a2,
+        const typename Derived::Scalar a3, const typename Derived::Scalar b,
+        const typename Derived::Scalar lconst,
+        const Eigen::ArrayBase<Derived>& lscf);
+
 #endif
