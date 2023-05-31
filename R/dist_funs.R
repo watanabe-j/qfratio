ed3bb6108877458b1af488d2968da36398d57e51
Junya Watanabe <jw2098@cam.ac.uk>
2023-05-31 17:23:35 +0100

    Add CompQuadForm versions for pqfr()

--- R/dist_funs.R
+++ R/dist_funs.R
@@ -6,12 +6,14 @@
 #' The user is supposed to use the exported functions \code{dqfr()} and
 #' \code{pqfr()}, which are (pseudo-)vectorized with respect to \code{quantile}
 #' using \code{\link[base]{sapply}()}.  The actual calculations are done by the
-#' *internal* functions \code{dqfr_A1I1()} and \code{pqfr_A1B1()} which only
-#' accommodates a length-one \code{quantile}.  The internal functions skip
+#' *internal* functions \code{dqfr_A1I1()}, \code{pqfr_A1B1()},
+#' \code{pqfr_imhof()}, and \code{pqfr_davies()} which only
+#' accommodate a length-one \code{quantile}.  The internal functions skip
 #' most checks on argument structures and do not accommodate \code{Sigma}
 #' (where applicable) to reduce execution time.
 #'
-#' Evaluate the probability density or (cumulative) distribution function
+#' \code{dqfr_A1I1()} and \code{pqfr_A1B1()} evaluate the probability density
+#' and (cumulative) distribution function, respectively,
 #' as a partial sum of infinite series involving top-order zonal or
 #' invariant polynomials.  The density is from Hillier
 #' (2001, corollary 2), the distribution function for the central case is
@@ -20,19 +22,40 @@
 #' this package, these are evaluated with the recursive algorithm for
 #' \eqn{d} in Hillier et al. (2009, 2014).
 #'
+#' \code{pqfr_imhof()} and \code{pqfr_davies()} evaluate the distribution
+#' function by numerical inversion of the characteristic function based on
+#' Imhof (1961) or Davies (1973, 1980), calling
+#' \code{\link[CompQuadForm]{imhof}()} and \code{\link[CompQuadForm]{davies}()},
+#' respectively, from the package \pkg{CompQuadForm}.  This is from the fact
+#' that, for nonnegative definite \eqn{\mathbf{B}}{B},
+#' \eqn{ P \left\{
+#'         \frac{ \mathbf{x^\mathit{T} A x} }{ \mathbf{x^\mathit{T} B x} }
+#'         > q \right\} =
+#'       P \left\{ \mathbf{x}^\mathit{T} (\mathbf{A} - q \mathbf{B}) \mathbf{x}
+#'         > 0 \right\} }{ P\{(x^T A x) / (x^T B x) > q\} =
+#'                         P\{x^T (A - q B) x > 0\}}, so that the distribution
+#' function of the ratio can be expressed in terms of that of the latter
+#' (indefinite) quadratic form.  (The same argument was used in the derivation
+#' of Forchini (2002, 2005) as well.)  Optional arguments for
+#' \code{\link[CompQuadForm]{imhof}()} and \code{\link[CompQuadForm]{davies}()}
+#' can be passed via \code{...}, except for \code{sigma} in the latter,
+#' which is not applicable.
+#'
 #' The density is undefined at the eigenvalues of \eqn{\mathbf{A}}{A}
 #' (Hillier 2001) and the distribution function has points of nonanalyticity
 #' at the relative eigenvalues of the argument matrices (Forchini 2002).  Around
-#' these points, the expression tends to be *very slow* to converge.  In
-#' this case, use a large \code{m}, or seek alternative ways of evaluation.
+#' these points, the series expression tends to be *very slow* to converge.  In
+#' this case, use a large \code{m}, avoid using \code{method = "forchini"} in
+#' \code{pqfr()}, or seek alternative ways of evaluation.
 #'
 #' @inheritParams qfrm
 #'
 #' @param quantile
 #'   Numeric vector of quantiles \eqn{q}
 #' @param A,B
-#'   Argument matrices.  Should be square.  Will be automatically symmetrized
-#'   in \code{pqfr()}.
+#'   Argument matrices.  Should be square.  \code{B} should be nonnegative
+#'   definite.  Will be automatically symmetrized in \code{dqfr()} and
+#'   \code{pqfr()}.
 #' @param LA
 #'   Eigenvalues of \eqn{\mathbf{A}}{|}
 #' @param Sigma
@@ -41,6 +64,17 @@
 #' @param log,lower.tail,log.p
 #'   Logical; as in regular probability distribution functions.  But these are
 #'   for convenience only, and not meant for accuracy.
+#' @param method
+#'   Method to specify an internal function in \code{pqfr()}
+#'   (see \dQuote{Details}).  Options:
+#'   \itemize{
+#'     \item{\code{"imhof"}: }{default; uses \code{pqfr_imhof()}, numerical
+#'           inversion of Imhof (1961)}
+#'     \item{\code{"davies"}: }{uses \code{pqfr_davies()}, numerical inversion
+#'           of Davies (1973, 1980)}
+#'     \item{\code{"forchini"}: }{uses \code{pqfr_A1B1()}, series expression
+#'           of Forchini (2002, 2005)}
+#'    }
 #' @param check_convergence
 #'   Specifies how numerical convergence is checked (see
 #'   \code{\link{qfrm}})
@@ -77,7 +111,25 @@
 #'  }
 #' Only \code{$d} or \code{$p} is passed to the external functions.
 #'
+#' \code{pqfr_imhof()} and \code{pqfr_davies()} simply return the full output
+#' (a list) of \code{CompQuadForm::\link[CompQuadForm]{imhof}()} and
+#' \code{CompQuadForm::\link[CompQuadForm]{davies}()}, respectively.  This may
+#' be useful for debugging purposes.  Note that
+#' \code{$Qq} there is the *upper* \eqn{p}-value.  When
+#' \code{lower.tail = TRUE}, \code{pqfr()} takes its complement to return
+#' the lower \eqn{p}-value.
+#'
 #' @references
+#' Davis, R. B. (1973) Numerical inversion of a characteristic function.
+#'   *Biometrika*, **60**, 415--417.
+#'   \doi{10.1093/biomet/60.2.415}.
+#'
+#' Davis, R. B. (1980) Algorithm AS 155: The distribution of a linear
+#'   combination of \eqn{\chi^2} random variables.
+#'   *Journal of the Royal Statistical Society, Series C---Applied Statistics*,
+#'   **29**, 323--333.
+#'   \doi{10.2307/2346911}.
+#'
 #' Forchini, G. (2002) The exact cumulative distribution function of
 #'   a ratio of quadratic forms in normal variables, with application to
 #'   the AR(1) model. *Econometric Theory*, **18**, 823--852.
@@ -102,6 +154,13 @@
 #'   short recursions, with applications to the moments of quadratic forms
 #'   in noncentral normal vectors. *Econometric Theory*, **30**, 436--473.
 #'   \doi{10.1017/S0266466613000364}.
+#'
+#' Imhof, J. P. (1961) Computing the distribution of quadratic forms in normal
+#'   variables.
+#'   *Biometrika*, **48**, 419--426.
+#'   \doi{10.1093/biomet/48.3-4.419}.
+#'
+#' @seealso \code{\link{rqfr}}, a Monte Carlo random number generator
 #'
 #' @name pqfr
 #'
@@ -141,10 +200,9 @@ NULL
 #'
 #' \code{pqfr()}: \sQuote{Exact} distribution function of the ratio of
 #' quadratic forms,
-#' \eqn{\frac{ (\mathbf{x^\mathit{T} A x}) }{ (\mathbf{x^\mathit{T} B x}) }
+#' \eqn{\frac{ \mathbf{x^\mathit{T} A x} }{ \mathbf{x^\mathit{T} B x} }
 #'      }{ (x^T A x) / (x^T B x) }, where
-#' \eqn{\mathbf{x} \sim N_n(\bm{\mu}, \mathbf{\Sigma})}{x ~ N_n(\mu, \Sigma)},
-#' based on Forchini (2002, 2005).
+#' \eqn{\mathbf{x} \sim N_n(\bm{\mu}, \mathbf{\Sigma})}{x ~ N_n(\mu, \Sigma)}.
 #'
 #' @rdname pqfr
 #' @order 2
@@ -153,9 +211,11 @@ NULL
 #'
 pqfr <- function(quantile, A, B, m = 100L, mu = rep.int(0, n), Sigma = diag(n),
                  lower.tail = TRUE, log.p = FALSE,
+                 method = c("imhof", "davies", "forchini"),
                  tol_zero = .Machine$double.eps * 100,
                  tol_sing = .Machine$double.eps * 100,
                  ...) {
+    method <- match.arg(method)
     ## If A or B is missing, let it be an identity matrix
     ## If they are given, symmetrize
     if(missing(A)) {
@@ -196,7 +256,7 @@ pqfr <- function(quantile, A, B, m = 100L, mu = rep.int(0, n), Sigma = diag(n),
             }
         }
         return(pqfr(quantile, KtAK, KtBK, m = m, mu = iKmu,
-                    lower.tail = lower.tail, log.p = log.p,
+                    lower.tail = lower.tail, log.p = log.p, method = method,
                     tol_zero = tol_zero, tol_sing = tol_sing, ...))
     }
     LB <- eigen(B, symmetric = TRUE)$values
@@ -206,11 +266,24 @@ pqfr <- function(quantile, A, B, m = 100L, mu = rep.int(0, n), Sigma = diag(n),
         "B must be nonnegative definite" = all(LB >= -tol_sing),
         "quantile must be numeric" = is.numeric(quantile)
     )
-    ans <- sapply(quantile, 
-                  function(q) pqfr_A1B1(q, A, B, m, mu = mu, 
-                                        tol_zero = tol_zero,
-                                        tol_sing = tol_sing, ...)$p)
-    if(!lower.tail) ans <- 1 - ans
+    if(method == "forchini") {
+        ans <- sapply(quantile,
+                      function(q) pqfr_A1B1(q, A, B, m, mu = mu,
+                                            tol_zero = tol_zero,
+                                            tol_sing = tol_sing, ...)$p)
+        if(!lower.tail) ans <- 1 - ans
+    } else {
+        if(method == "davies") {
+            ans <- sapply(quantile,
+                          function(q) pqfr_davies(q, A, B, mu = mu,
+                                                  tol_zero = tol_zero, ...)$Qq)
+        } else {
+            ans <- sapply(quantile,
+                          function(q) pqfr_imhof(q, A, B, mu = mu,
+                                                 tol_zero = tol_zero, ...)$Qq)
+        }
+        if(lower.tail) ans <- 1 - ans
+    }
     if(log.p) ans <- log(ans)
     attributes(ans) <- attributes(quantile)
     return(ans)
@@ -219,7 +292,8 @@ pqfr <- function(quantile, A, B, m = 100L, mu = rep.int(0, n), Sigma = diag(n),
 ##### pqfr_A1B1 #####
 #' Probability distribution of ratio of quadratic forms
 #'
-#' \code{pqfr_A1B1()}: internal function used within \code{pqfr()}.
+#' \code{pqfr_A1B1()}: internal function used within \code{pqfr()},
+#' based on Forchini (2002, 2005).
 #'
 #' @rdname pqfr
 #' @order 4
@@ -357,15 +431,86 @@ pqfr_A1B1 <- function(quantile, A, B, m = 100L,
     list(p = sum(ansseq), terms = ansseq)
 }
 
+##### pqfr_imhof #####
+#' Probability distribution of ratio of quadratic forms
+#'
+#' \code{pqfr_imhof()}: internal function used within \code{pqfr()},
+#' based on Imhof (1961).
+#'
+#' @rdname pqfr
+#' @order 5
+#'
+pqfr_imhof <- function(quantile, A, B, mu = rep.int(0, n),
+                       tol_zero = .Machine$double.eps * 100, ...) {
+    if(!requireNamespace("CompQuadForm", quietly = TRUE)) {
+        stop("Package 'CompQuadForm' is required to use this function")
+    }
+    ## If A or B is missing, let it be an identity matrix
+    if(missing(A)) {
+        if(missing(B)) stop("Provide at least one of A and B")
+        n <- dim(B)[1L]
+        A <- diag(n)
+    } else {
+        n <- dim(A)[1L]
+        if(missing(B)) B <- diag(n)
+    }
+    ## Check basic requirements for arguments
+    stopifnot(
+        "In pqfr_imhof, quantile must be length-one" = (length(quantile) == 1)
+    )
+    eigA_qB <- eigen(A - quantile * B, symmetric = TRUE)
+    lambda <- eigA_qB$values
+    delta2 <- c(crossprod(eigA_qB$vectors, mu)) ^ 2
+    if(all(lambda <=  tol_zero)) return(list(Qq = 0, abserr = 0))
+    if(all(lambda >= -tol_zero)) return(list(Qq = 1, abserr = 0))
+    CompQuadForm::imhof(0, lambda = lambda, h = rep.int(1, n),
+                        delta = delta2, ...)
+}
+
+##### pqfr_davies #####
+#' Probability distribution of ratio of quadratic forms
+#'
+#' \code{pqfr_davies()}: internal function used within \code{pqfr()},
+#' based on Davies (1973, 1980).
+#'
+#' @rdname pqfr
+#' @order 6
+#'
+pqfr_davies <- function(quantile, A, B, mu = rep.int(0, n),
+                        tol_zero = .Machine$double.eps * 100, ...) {
+    if(!requireNamespace("CompQuadForm", quietly = TRUE)) {
+        stop("Package 'CompQuadForm' is required to use this function")
+    }
+    ## If A or B is missing, let it be an identity matrix
+    if(missing(A)) {
+        if(missing(B)) stop("Provide at least one of A and B")
+        n <- dim(B)[1L]
+        A <- diag(n)
+    } else {
+        n <- dim(A)[1L]
+        if(missing(B)) B <- diag(n)
+    }
+    ## Check basic requirements for arguments
+    stopifnot(
+        "In pqfr_davies, quantile must be length-one" = (length(quantile) == 1)
+    )
+    eigA_qB <- eigen(A - quantile * B, symmetric = TRUE)
+    lambda <- eigA_qB$values
+    delta2 <- c(crossprod(eigA_qB$vectors, mu)) ^ 2
+    if(all(lambda <=  tol_zero)) return(list(trace = rep.int(0, 7), ifault = 0, Qq = 0))
+    if(all(lambda >= -tol_zero)) return(list(trace = rep.int(0, 7), ifault = 0, Qq = 1))
+    CompQuadForm::davies(0, lambda = lambda, h = rep.int(1, n),
+                         delta = delta2, sigma = 0, ...)
+}
+
 
 ##### dqfr #####
 #' Probability distribution of ratio of quadratic forms
 #'
 #' \code{dqfr()}: \sQuote{Exact} density of the ratio of quadratic forms,
-#' \eqn{\frac{ (\mathbf{x^\mathit{T} A x}) }{ (\mathbf{x^\mathit{T} x}) }
+#' \eqn{\frac{ \mathbf{x^\mathit{T} A x} }{ \mathbf{x^\mathit{T} x} }
 #'      }{ (x^T A x) / (x^T x) }, where
-#' \eqn{\mathbf{x} \sim N_n(\mathbf{0}_n, \mathbf{I}_n)}{x ~ N_n(0_n, I_n)},
-#' based on Hillier (2001).
+#' \eqn{\mathbf{x} \sim N_n(\mathbf{0}_n, \mathbf{I}_n)}{x ~ N_n(0_n, I_n)}.
 #'
 #' @rdname pqfr
 #' @order 1
@@ -394,7 +539,8 @@ dqfr <- function(quantile, A, m = 100L, log = FALSE, ...) {
 ##### dqfr_A1I1 #####
 #' Probability distribution of ratio of quadratic forms
 #'
-#' \code{dqfr_A1I1()}: internal function used within \code{dqfr()}.
+#' \code{dqfr_A1I1()}: internal function used within \code{dqfr()},
+#' based on Hillier (2001).
 #'
 #' @rdname pqfr
 #' @order 3
