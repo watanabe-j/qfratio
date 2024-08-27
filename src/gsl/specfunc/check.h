e5dbf82efb329522e094ebdafa61e18bf7302b2b
Junya Watanabe <Junya.Watanabe@uab.cat>
2024-08-27 15:21:03 +0200

    Delete unnecessary codes and configure scripts

--- src/gsl/specfunc/check.h
+++
@@ -1,8 +0,0 @@
-// This file is taken from GSL version 2.7.1 and distributed as part of qfratio
-// with modification, in accordance with the GNU General Public License
-// version 3.  All modified lines are marked with comments.
-// - 2023 Junya Watanabe
-
-/* check for underflow */
-
-#define CHECK_UNDERFLOW(r) if (fabs((r)->val) < GSL_DBL_MIN) GSL_ERROR("underflow", GSL_EUNDRFLW);
